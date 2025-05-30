81ad43513667cbdd99743af7c40f2af0d18765d2
Harry <harry.hayward@praetorian.com>
2025-05-29 21:31:37 -0400

    Fix Vercel deployment errors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-  },
+  // Remove the experimental optimizeCss setting
 }
 
 module.exports = nextConfig
